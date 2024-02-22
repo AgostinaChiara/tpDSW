e4d75f4e0bb131692abbfac7d21ef58ba98940c8
Agostina Chiara <agosschiara25@gmail.com>
2024-02-21 21:44:15 -0300

    commit

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,7 +1,6 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { UserService } from 'src/app/services/user.service';
 
